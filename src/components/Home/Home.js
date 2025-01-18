6c8280e5e90e41de5773c3886b27e124ef99f9e3
Mohammed Abdullah Almuhaitheef <141905441+Muhaitheef@users.noreply.github.com>
2025-01-18 20:33:36 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> MOHAMMED ALMUHAITHEEF</strong>
+                <strong className="main-name"> MOHAMMED AL-MUHAITHEEF</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "center", marginLeft: "20px" }}>
